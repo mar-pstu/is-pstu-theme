1de92a19a7785f6636153b5b9b6daf8a792b1db3
chomovva <chomovva@gmail.com>
2021-12-22 16:20:09 +0200

    добавил кнопку копировать url в бефер + мелкие правки

--- scripts/public.js
+++ scripts/public.js
@@ -1,3 +1,6 @@
+/**
+ * Кнопка мобильного меню
+ * */
 ( function () {
 
 	let $list = jQuery( '#nav-list' );
@@ -7,6 +10,16 @@
 		$list.toggleClass( 'active' );
 	}
 
+	$burger.on( 'click', Toggle );
+
+} )();
+
+
+/**
+ * Стили фильтра приказов
+ * */
+( function () {
+
 	function ControlStyleToggle() {
 		if ( jQuery( this ).val().length ) {
 			jQuery( this ).addClass( 'changed' );
@@ -15,7 +28,6 @@
 		}
 	}
 
-	$burger.on( 'click', Toggle );
 	jQuery( document ).on( 'change', 'select, input, trextarea', ControlStyleToggle );
 	jQuery( document ).on( 'keyup', 'input, trextarea', ControlStyleToggle );
 	jQuery( document ).ready( function () {
@@ -25,5 +37,43 @@
 			}
 		} );
 	} );
+
+} )();
+
+
+/**
+ * Копировать в буфер
+ * */
+( function () {
+
+	let $body = jQuery( 'body' );
+
+	function Copy( event ) {
+		event.preventDefault();
+		let $button = jQuery( event.target );
+		let $textarea = jQuery( '<textarea>' );
+		let link = $button.attr( 'data-link' );
+		let label = $button.text();
+		let success = $button.attr( 'data-success' );
+		console.log( $button );
+		if ( typeof link !== 'undefined' && link !== false ) {
+			$body.append( $textarea );
+			$textarea.val( $button.attr( 'data-link' ) );
+			$textarea.select();
+			document.execCommand( 'copy' );
+			$textarea.remove();
+			console.log( 'copy' );
+			$body.off( 'click', 'button.link', Copy );
+			if ( typeof success !== 'undefined' && success !== false ) {
+				$button.text( success );
+				setTimeout( function () {
+					$button.text( label );
+					$body.on( 'click', 'button.link', Copy );
+				}, 1500 );
+			}
+		}
+	}
+
+	$body.on( 'click', 'button.share', Copy );
 
 } )();
